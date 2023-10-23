2947ad24137dfb66a384cf6114909153c4422dae
Anshika Singh <111517535+anshikagith@users.noreply.github.com>
2023-10-23 17:53:51 +0000

    datasum

---
+++ 1_basics/datatype_summ.js
@@ -0,0 +1,17 @@
+/*
+Primitive-
+    7 types- string, number, symbol, boolean, undefined, bigint, null
+Non primitve
+arrays, objects, functions
+const num={1,2,3} //arrays
+//obj
+let obj ={
+    name: "anshika"
+    age: 20
+}
+const myfun m=function(){
+
+}
+
+ *****Check ECMA documentation for type of datatypes****
+*/
