18c731cfd5cee384627c431327fa123672de7778
Anshika Singh <111517535+anshikagith@users.noreply.github.com>
2023-10-23 17:11:47 +0000

    commit

---
+++ .devcontainer/1_basics/variables.js
@@ -0,0 +1,5 @@
+const x=1224 //cant be changed
+let name="Anshika"  //always used for declaring variable
+var password=12345 //not used because of scope issues
+email="anshika@gmail.com" //another way of declaring a variable but this is not preferred
+console.log(x)
