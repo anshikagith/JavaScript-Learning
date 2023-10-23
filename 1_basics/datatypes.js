51a3e75e3f3d3a8e227a27a72839a8c7160e4cbb
Anshika Singh <111517535+anshikagith@users.noreply.github.com>
2023-10-23 17:21:44 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+/*
+datatypes:
+number
+string
+boolean
+null - standalone value
+undefined
+
+objects
+
+*/
+console.log(typeof "Anshika");
+console.log(typeof null); //object
+console.log(typeof undefined) //undefined
