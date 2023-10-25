9aece9097718668e4903da228e24f52d3f9aa1bd
Anshika Singh <111517535+anshikagith@users.noreply.github.com>
2023-10-25 16:30:08 +0000

    functions

---
+++ 3_basics/functions.js
@@ -0,0 +1,36 @@
+function add(num1, num2){
+    let a=num1 + num2
+    return a
+}
+console.log(add(60,9));
+
+function greeting(user) {
+    return `${user} says hi`
+}
+console.log(greeting("Anshika"));
+//if no argument is given 
+ console.log(greeting()); //undefined says hi
+
+ //if we want to pass multiple arguments for same variable
+
+ function cart(...value) {
+    return value
+ }
+ console.log(cart(200,300,400)); //200 300 400
+//important
+ function cart(num1,num2,...value) {
+    return value
+ }
+ console.log(cart(200,300,400,500)); // [400, 500]
+
+ 
+ //functions and objects
+
+ const ob = {
+    name : "Anshi",
+    age : 20
+ }
+ function obfun(anyob) {
+    console.log(`Name : ${anyob.name} Age : ${anyob.age}`);
+ }
+ obfun(ob)
